122613a4063211fb9fda5ab266ba48ab6ef7baed
Vadym Prokhorenko <prokhorenko.vadim.98@gmail.com>
2023-04-09 22:20:50 +0300

    Add event no scroll from backdrop

--- js/modal.js
+++ js/modal.js
@@ -8,8 +8,10 @@
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
     refs.modal.addEventListener("click", (click) => {
-        if (click.target === click.currentTarget)
+        if (click.target === click.currentTarget) {
             refs.modal.classList.add("is-hidden");
+            document.body.classList.toggle("no-scroll");
+        }
     });
     function toggleModal() {
         refs.modal.classList.toggle("is-hidden");
