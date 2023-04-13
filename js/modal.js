45f39bf83328b05dcc1bb1405ce1ce32618ff8d9
Vadym Prokhorenko <prokhorenko.vadim.98@gmail.com>
2023-04-13 14:04:04 +0300

    refactor function

--- js/modal.js
+++ js/modal.js
@@ -4,14 +4,10 @@
         closeModalBtn: document.querySelector("[data-modal-close]"),
         modal: document.querySelector("[data-modal]"),
     };
-
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
-    refs.modal.addEventListener("click", (click) => {
-        if (click.target === click.currentTarget) {
-            refs.modal.classList.add("is-hidden");
-            document.body.classList.toggle("no-scroll", false);
-        }
+    refs.modal.addEventListener("click", click => {
+        if(click.target === click.currentTarget) toggleModal();
     });
     function toggleModal() {
         refs.modal.classList.toggle("is-hidden");
